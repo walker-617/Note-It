5e9a13e3aeda69f5f189f87f2c7324545900a568
WALKER <walker.1100100@gmail.com>
2024-03-31 14:40:58 +0530

    added config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyDlAp3fyy6RnE39OoYbUzHtjvgyv7CEGjg",
+    authDomain: "my-notes-2964d.firebaseapp.com",
+    projectId: "my-notes-2964d",
+    storageBucket: "my-notes-2964d.appspot.com",
+    messagingSenderId: "684231297125",
+    appId: "1:684231297125:web:b2ff1bc6057f6471391e8c",
+    measurementId: "G-N72GKW3VNM",
+  };
+  
+  export { config };
